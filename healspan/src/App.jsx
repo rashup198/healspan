1ed67a334eb939c23bd8af848f851ad38ba77698
Priyanshu Pandey <priyanshukum198@gmail.com>
2024-01-04 11:23:19 +0530

    Add OurClients component to App.jsx

--- healspan/src/App.jsx
+++ healspan/src/App.jsx
@@ -3,6 +3,7 @@ import './index.css';
 import Navbar from './components/navbar';
 import { Link } from 'react-router-dom';
 import Hero from './components/Hero';
+import OurClients from './components/OurClients';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="h-[100vh]"> 
         <Navbar />
         <Hero />
+        <OurClients></OurClients>
       </div>
     </>
   );
---
+++ healspan/src/components/OurClients.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import img1 from "../assets/hospital/APOLLOHOSP.NS_BIG.png"
+import img2 from "../assets/hospital/dr-agarwals-eye-hospital-logo-940BA4DA1B-seeklogo.com.png"
+import img3 from "../assets/hospital/LALPATHLAB.NS_BIG.png"
+import img4 from "../assets/hospital/MAXHEALTH.NS_BIG.png"
+
+const OurClients = () => {
+  return (
+    <div>
+      <div className="parent flex flex-col justify-center items-center ">
+        <h1 className='text-[40px] font-semibold pt-[30px] pb-[50px]'>
+            Trusted By The India's Leading Hospitals
+        </h1>
+        <div className="childParent flex gap-[100px] ">
+            <img src={img1} className="child w-[100px]"></img>
+            <img src={img2} className="child w-[250px]"></img>
+            <img src={img3} className="child w-[200px]"></img>
+            <img src={img4} className="child w-[270px]"></img>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default OurClients
