defd27656c9f323e495eee890cb6671e5fb95f85
Priyanshu Kumar Pandey <88549100+rashup198@users.noreply.github.com>
2024-02-26 13:09:09 +0530

    Added sendOTp

--- healspan/server/controllers/Auth.js
+++ healspan/server/controllers/Auth.js
@@ -81,3 +81,119 @@ exports.signup= async(req,res)=>{
         })
     }
 }
+
+
+// login controller
+
+exports.login= async (req, res)=>{
+    try {
+
+        const {email, password}= req.body;
+
+        if(!email || !password){
+            return res.status(400).json({
+                success:false,
+                message:"enter all fields"
+            })
+        }
+
+        // find user
+
+        const user = await User.findOne({email}).populate("additionalDetails")
+        console.log(user);
+
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message:"User is not registered, first create account then login"
+            })
+        }
+
+        // genrate token 
+        if( await bcrypt.compare(password, user.password)){
+            const token = jwt.sign(
+                {
+                    email:user.email, id:user._id, role:user.accountType
+                }, process.env.JWT_SECRET,
+                {
+                    expriresIn:"24h",
+                }
+            )
+
+            user.token=token
+            user.password=undefined
+
+            //settinf cookies
+
+            const options ={
+                exprires:new Date(Date.now()+500000),
+                httpOnly:true,
+            }
+            res.cookie("token", token, options).status(200).json({
+                success: true,
+                token,
+                user,
+                message:"User Logged in Sucessfully"
+            })
+        }else{
+            return res.status(400).json({
+                success:false,
+                message:"password is wrong"
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:"something went wrong try again"
+        })
+    }
+}
+
+// otp
+
+exports.sendotp= async(req, res)=>{
+    try {
+        const {email}= req.body;
+
+        const userPresent = await User.findOne({email});
+
+        if(userPresent){
+            return res.status(400).json({
+                success:false,
+                message:"User is already registerd"
+            })
+        }
+        const otp = otpGenerator.generate(6,{
+            upperCaseAlphabets: false,
+            lowerCaseAlphabets: false,
+            specialChars: false,
+        })
+        const result = await OTP.findOne({otp:otp})
+        console.log("the otp", otp);
+        console.log("the result",result);
+
+        while (result) {
+            otp = otpGenerator.generate(6, {
+              upperCaseAlphabets: false,
+              lowerCaseAlphabets: false,
+              specialChars: false,
+            })
+          }
+          const otpPayload = { email, otp }
+          const otpBody = await OTP.create(otpPayload)
+          console.log("OTP Body", otpBody)
+          res.status(200).json({
+            success: true,
+            message: `OTP Sent Successfully`,
+            otp,
+          })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success :false,
+            error:error.message,
+            message:"Can not send otp try again"
+        })
+    }
+}
