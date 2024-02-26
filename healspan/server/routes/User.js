4416265860ac41d6524ea3230436791179deb2c4
Priyanshu Kumar Pandey <88549100+rashup198@users.noreply.github.com>
2024-02-27 03:07:19 +0530

    updated user route

--- healspan/server/routes/User.js
+++ healspan/server/routes/User.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { login, signup, sendotp, changePassword } = require("../controllers/Auth");
+const {auth} =require("../middelwares/auth")
 
 router.post("/login", login);
 router.post("/signup", signup);
 router.post("/sendotp", sendotp);
-router.post("/changepassword", changePassword);
+router.post("/changepassword", auth,changePassword);
 
 module.exports = router; 
