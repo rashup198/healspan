1e7f4d9d633cbd05734625406814a2e9d2d31109
Priyanshu Kumar Pandey <88549100+rashup198@users.noreply.github.com>
2024-02-28 02:48:06 +0530

    updated

--- healspan/server/controllers/Claim.js
+++ healspan/server/controllers/Claim.js
@@ -51,3 +51,96 @@ exports.submitClaim = async (req, res) => {
         });
     }
 }
+
+// view claim details
+
+exports.getClaimDetail= async(req,res)=>{
+    try {
+       const userId= req.user.id;
+       if(!userId){
+        return res.status(400).json({
+            success:false,
+            message:"User Not found"
+        })
+       }
+
+       const {claimId}= req.body;
+       if(!claimId){
+        return res.status(400).json({
+            success:false,
+            message:"Enter claim id"
+        })
+       }
+
+       const claimDetails = await Claim.findById(claimId);
+       if(!claimDetails){
+        return res.status(404).json({
+            success:false,
+            message:"Claim not found"
+        })
+       }
+
+       return res.status(200).json({
+        success:true,
+        message:"Claim details fetched Successfully",
+        data:claimDetails
+       })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message:"failed to fetch Claim details",
+            error: error.message
+        })
+    }
+}
+
+// edit claim detail 
+
+exports.editClaim= async (req,res)=>{
+    try {
+        const{claimId}= req.body;
+        const updates =req.body;
+        const claim = await Claim.findById(claimId);
+
+        if(!claim){
+            return res.status(400).json({
+                success:false,
+                message:"Claim not found"
+            })
+        }
+
+        if(req.files){
+            console.log("bill Image");
+            const billPhoto = req.files.billPhoto;
+            const medicalReport= req.files.medicalReport;
+            const billUpload = await uploadImageToCloudinary(billPhoto, process.env.FOLDER_NAME);
+            const medicalUpload = await uploadImageToCloudinary(medicalReport, process.env.FOLDER_NAME)
+            claim.billPhoto=billUpload.secure_url;
+            claim.medicalReport= medicalUpload.secure_url;
+        }
+
+        for(const key in updates){
+            claim[key]= updates[key];
+        }
+
+        await claim.save();
+
+        const updatedClaim = await Claim.findOne({_id:claimId});
+
+        return res.status(200).json({
+            success:true,
+            message:"Claim details updted Sucessfully",
+            data:updatedClaim
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message:"failed to update Claim details",
+            error: error.message
+        })
+    }
+}
+
--- healspan/server/routes/Claim.js
+++ healspan/server/routes/Claim.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router(); 
 const { auth } = require("../middelwares/auth"); 
-const {submitClaim} = require("../controllers/Claim");
+const {submitClaim,getClaimDetail,editClaim} = require("../controllers/Claim");
 
 router.post("/submit-claim", auth,submitClaim);
+router.post("/get-claim-detail",auth,getClaimDetail);
+router.put("/edit-claim_detail",auth,editClaim);
 
 module.exports = router;
